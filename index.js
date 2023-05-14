99e0deffd0f7eb5941fccbcade0cb08ea443e610
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-05-14 19:22:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 const contacts = require("./contacts");
-const argv = require("yargs").argv;
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -18,6 +29,9 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
     case "remove":
       const removeById = await contacts.removeContact(id);
       return console.log(removeById);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
